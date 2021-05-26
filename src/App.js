10bad947475a8b91e791c3690a7dbc8ee5a7f2f0
Skrekliam <typnickman@gmail.com>
2021-05-26 16:31:40 +0300

    Add axios api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 import { Container } from 'react-bootstrap';
 import Employees from './components/Employees';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from 'react';
 
 function App() {
 
-  
+  useEffect(()=>{
+    axios.get('https://yalantis-react-school-api.yalantis.com/api/task0/users').then(res=> console.log(res))
+  },[])
 
 
 
