80776bbf2b9587ff4ea9de5678921efd9d5324a2
Skrekliam <typnickman@gmail.com>
2021-05-30 15:46:17 +0300

    Add radiobuttons

--- src/components/EmployeeItem.js
+++ src/components/EmployeeItem.js
@@ -1,7 +1,7 @@
-import React, { useState,useEffect } from "react";
-import { Card, Col, Row } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { ButtonGroup, Card, Col, Form, InputGroup, Row, ToggleButton } from "react-bootstrap";
 import { useSelector } from "react-redux";
-import { store } from '../index.js';
+import { store } from "../index.js";
 
 function EmployeeItem() {
   console.log("key");
@@ -10,6 +10,7 @@ function EmployeeItem() {
   const [list, setlist] = useState(startlist);
   const [keys, setkeys] = useState([]);
   const [vals, setvals] = useState([]);
+  const [radioValue, setRadioValue] = useState({});
 
   const handleList = () => {
     const newState = store.getState().list;
@@ -18,11 +19,11 @@ function EmployeeItem() {
     setvals(Object.values(list));
   };
   store.subscribe(handleList);
-  
+
   let eList = [];
   // console.log(list[keys[0]]);
   console.log(vals);
-  let cnt=0;
+  let cnt = 0;
   //   console.log(list.A)
 
   //   console.log(eList)
@@ -34,14 +35,26 @@ function EmployeeItem() {
         <div>
           {vals.map((el1, ind) => {
             return (
-              <Card key={cnt++} border="info" style={{width: '18rem' }}>
-                <Card.Header >{keys[ind]}</Card.Header>
+              <Card key={cnt++} border="info" style={{ width: "18rem" }}>
+                <Card.Header>{keys[ind]}</Card.Header>
                 <Card.Body>
-               {el1.map(el2 => { console.log(el2); 
-                    return (<div key={el2['id']}> <Card.Title >{el2['lastName']} {el2['firstName']}</Card.Title>
-                <Card.Text><button>1</button><button>2</button></Card.Text>
-               </div>)
-                })}
+                  {el1.map((el2) => {
+                    console.log(el2);
+                    return (
+                      <div key={el2["id"]}>
+                        {" "}
+                        <Card.Title>
+                          {el2["lastName"]} {el2["firstName"]}
+                        </Card.Title>
+                        <Card.Text>
+                         
+                          <input type="radio" name={el2['id']} value="not active" id={el2['id']} /> Not active <br />
+                          <input type="radio" name={el2['id']} value="active" id={el2['id']} /> Active <br />
+                        </Card.Text>
+                        <hr />
+                      </div>
+                    );
+                  })}
                 </Card.Body>
               </Card>
             );
--- src/reducers/employeesReducer.js
+++ src/reducers/employeesReducer.js
@@ -3,7 +3,9 @@ const employeeReducer = (state = [], action) => {
     case "ADD_EMPLOYEES":
           for (const el of action.payload) {
             let ind = el['lastName'][0];
+            if(state[ind]){
             state[ind].push(el);
+            }
           // console.log(action.payload[0]['lastName'][0])
           }
           return state;
